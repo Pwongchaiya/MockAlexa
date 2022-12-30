542c04e229047661cad424e165a0cea14946c9b9
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2022-12-29 21:39:58 -0800

    Update Alexa.java

--- untitled1/src/Alexa.java
+++ untitled1/src/Alexa.java
@@ -8,18 +8,20 @@ public class Alexa {
     this.door = door;
   }
 
+
+  public void setLight(boolean bool){
+    lightSwitch.setState(bool);
+  }
+
+  public void setDoor(Boolean bool){
+    door.setState(bool);
+  }
+
   public void doorStatus(){
     System.out.println("The Door is: "+door.getState());
   }
 
   public void lightStatus(){
     System.out.println("The Light is: "+lightSwitch.getState());
-  }
-    public void setLight(String bool){
-    lightSwitch.setState(bool);
-  }
-
-  public void setDoor(String bool){
-    door.setState(bool);
   }
 }
